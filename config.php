848415a36efb30ea35f00eaeb7f1f19f614bb52f
Jarne Eldrup <s143700@ap.be>
2024-03-23 14:42:58 +0100

    -

--- config.php
+++ config.php
@@ -7,4 +7,11 @@ $databaseName = 'milage';
 
 // Connect to the database
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+// Check if the connection was successful
+if (!$mysqli) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connection successful!";
+}
 ?>
