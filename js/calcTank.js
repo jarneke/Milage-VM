d839946976b51b8f14675ef6de574836b3c335d0
Jarne Eldrup <s143700@ap.be>
2024-03-24 17:01:07 +0100

    -

--- js/calcTank.js
+++ js/calcTank.js
@@ -1,11 +1,10 @@
-console.log("TEST");
-setTimeout(() => {
+window.onload = () => {
     const calculateButtons = document.getElementsByClassName('CalculateTank');
     for (const button of calculateButtons) {
         button.addEventListener('click', () => {
             const tankDiv = button.closest('.tank');
-            console.log(tankDiv.getAttribute("data-id"));
             if (tankDiv) {
+
                 const firstElementTextContent = tankDiv.firstElementChild.textContent;
                 const clickedFillDate = firstElementTextContent.split(" ")[2];
                 console.log("clicked Date: " + clickedFillDate);
@@ -13,4 +12,4 @@ setTimeout(() => {
             }
         });
     }
-}, 1000);
+};
