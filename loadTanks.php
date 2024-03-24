53eff46969dbc5f3c01b8676b3ed0abc878d9c0d
Jarne Eldrup <s143700@ap.be>
2024-03-24 16:33:52 +0100

    -

--- loadTanks.php
+++ loadTanks.php
@@ -8,11 +8,12 @@ $result = $mysqli->query($sql); // Use $mysqli instead of $conn
 
 if ($result->num_rows > 0) {
     // Output data of each row
+    int i = 0;
     while($row = $result->fetch_assoc()) {
-        echo "<div class='tank'>";
+        echo '<div class='tank' data-id="' . i . '">';
         echo "<p>Tank Date: " . $row["fillDate"] . "</p>";
         echo "<p>Cost: $" . $row["cost"] . "</p>";
-        echo '<button type="button">Calculate tank</button>';
+        echo '<button type="button" class="CalculateTank">Calculate tank</button>';
         echo "</div>";
     }
 } else {
