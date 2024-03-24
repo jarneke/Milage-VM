46ab0c94f3ac6ae04f3773c4b046f64247022daa
Jarne Eldrup <s143700@ap.be>
2024-03-24 17:40:27 +0100

    -

--- loadTripsBetween.php
+++ loadTripsBetween.php
@@ -12,13 +12,13 @@ $result = $mysqli->query($sql); // Use $mysqli instead of $conn
 
 if ($result->num_rows > 0) {
     // Output data of each row
-    $i = 0;
     while($row = $result->fetch_assoc()) {
         echo "<div class='trip'>";
         echo "<p>User: " . $row[user] . "</p>";
         echo "<p>tripDate: " . $row[tripDate] . "</p>";
         echo "<p>sm: " . $row[sm] . "</p>";
         echo "<p>em: " . $row[em] . "</p>";
+        echo "</div>";
     }
 } else {
     echo "0 results";
