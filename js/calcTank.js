0d78899a3c049e05e6ea733e77e52fd6e8110c02
Jarne Eldrup <s143700@ap.be>
2024-03-24 17:47:44 +0100

    -

--- js/calcTank.js
+++ js/calcTank.js
@@ -8,6 +8,17 @@ setTimeout(() => {
                 const clickedFillDate = tankDiv.firstElementChild.textContent.split(" ")[2];
                 console.log("clicked Date: " + clickedFillDate);
                 console.log("previos Date: " + previosFillDate);
+                let xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+                        document.getElementById("calcTank").innerHTML = this.responseText;
+                    }
+                };
+                xhttp.open("GET", "loadTripsBetween.php", true);
+                xhttp.send({
+                    curr: clickedFillDate,
+                    prev: previosFillDate
+                });
             }
         });
     }
