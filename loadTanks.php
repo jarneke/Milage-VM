abffd22b4ba8474079531986c4155e2c4cd576aa
Jarne Eldrup <s143700@ap.be>
2024-03-24 22:47:13 +0100

    -

--- loadTanks.php
+++ loadTanks.php
@@ -11,8 +11,8 @@ if ($result->num_rows > 0) {
     $i = 0;
     while($row = $result->fetch_assoc()) {
         echo "<div class='tank card col-12 col-md-5 col-lg-3 bg-secondary text-white' data-id='" . $i . "'>";
-        echo "<p>Tank Date: " . $row["fillDate"] . "</p>";
-        echo "<p>Cost: €" . $row["cost"] . "</p>";
+        echo '<p class="bg-secondary text-white">Tank Date: ' . $row["fillDate"] . '</p>';
+        echo '<p class="bg-secondary text-white">Cost: €' . $row["cost"] . '</p>';
         echo '<button type="button" class="CalculateTank">Calculate tank</button>';
         echo "</div>";
         $i++;
