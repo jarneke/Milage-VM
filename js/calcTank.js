78fdf38b79a24369944bfcc0789222d792b289a3
Jarne Eldrup <s143700@ap.be>
2024-03-24 16:44:58 +0100

    -

--- js/calcTank.js
+++ js/calcTank.js
@@ -1,7 +1,9 @@
 const calculateButtons = document.getElementsByClassName('CalculateTank');
-
+let i = 0;
 for (const button of calculateButtons) {
+    console.log(`button ${i} trying to add listener`);
     button.addEventListener('click', () => {
+        console.log(`button ${i} clicked`);
         const tankDiv = button.closest('.tank');
         if (tankDiv) {
             const firstElementTextContent = tankDiv.firstElementChild.textContent;
